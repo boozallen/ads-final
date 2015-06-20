e7bc5b8d698f9d4da96adbd11115f8497ac1c2d4
Sameer Dhar <avielus@gmail.com>
2015-06-20 17:56:08 -0400

    Fix bootstrap glyphicons

--- www/docs/js/docs-setup.js
+++ www/docs/js/docs-setup.js
@@ -2,62 +2,7 @@ NG_DOCS={
   "sections": {
     "api": "API Documentation"
   },
-  "pages": [
-    {
-      "section": "api",
-      "id": "gapFront",
-      "shortName": "gapFront",
-      "type": "overview",
-      "moduleName": "gapFront",
-      "shortDescription": "gapFront",
-      "keywords": "api application gapfront main module overview"
-    },
-    {
-      "section": "api",
-      "id": "gapFront.constant:CONSTANTS",
-      "shortName": "CONSTANTS",
-      "type": "function",
-      "moduleName": "gapFront",
-      "shortDescription": "CONSTANTS",
-      "keywords": "api config constant constants file function gapfront"
-    },
-    {
-      "section": "api",
-      "id": "gapFront.controller:AboutCtrl",
-      "shortName": "AboutCtrl",
-      "type": "function",
-      "moduleName": "gapFront",
-      "shortDescription": "AboutCtrl",
-      "keywords": "aboutctrl api controller function gapfront"
-    },
-    {
-      "section": "api",
-      "id": "gapFront.controller:ChartCtrl",
-      "shortName": "ChartCtrl",
-      "type": "function",
-      "moduleName": "gapFront",
-      "shortDescription": "ChartCtrl",
-      "keywords": "api chartctrl controller function gapfront"
-    },
-    {
-      "section": "api",
-      "id": "gapFront.controller:HeaderCtrl",
-      "shortName": "HeaderCtrl",
-      "type": "function",
-      "moduleName": "gapFront",
-      "shortDescription": "HeaderCtrl",
-      "keywords": "api controller function gapfront headerctrl"
-    },
-    {
-      "section": "api",
-      "id": "gapFront.controller:MainCtrl",
-      "shortName": "MainCtrl",
-      "type": "function",
-      "moduleName": "gapFront",
-      "shortDescription": "MainCtrl",
-      "keywords": "api controller function gapfront mainctrl"
-    }
-  ],
+  "pages": [],
   "apis": {
     "api": true
   },
