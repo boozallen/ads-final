798bc3ad5b4c852a1250acf48a6f364490ce9e00
Sami Mirimiri <mirimiri_sami@bah.com>
2015-06-22 11:20:37 -0400

    Updating style for single page auto-scroll

--- www/app/features/main/main.js
+++ www/app/features/main/main.js
@@ -16,10 +16,21 @@ angular.module('gapFront')
       'AngularJS',
       'Karma'
     ];
+
+    $scope.drugs = [
+      'Advil',
+      'Xanax',
+      'Diarrhea'
+    ];
+
+$scope.selectedState = "";
+$scope.states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
+
+$scope.selectedAddress = "";
+
     Restangular.setBaseUrl('mockJson/api/v1');
 		Restangular.all('medicineSearch.json').getList().then(function (response){
 			console.log(response);
-
 		});
     // console.log(CONSTANTS.testUrl);
   });
