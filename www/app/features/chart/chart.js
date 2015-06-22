d90696f8716eeee247893032e4f66e310619b81d
Sami Mirimiri <mirimiri_sami@bah.com>
2015-06-22 12:56:42 -0400

    updated chart example

--- www/app/features/chart/chart.js
+++ www/app/features/chart/chart.js
@@ -15,129 +15,124 @@ angular.module('gapFront')
       'Karma'
     ];
 
-    
-var data = {
-  labels: [
-    'resilience', 'maintainability', 'accessibility',
-    'uptime', 'functionality', 'impact'
-  ],
-  series: [
-    {
-      label: '2012',
-      values: [4, 8, 15, 16, 23, 42]
-    },
-    {
-      label: '2013',
-      values: [12, 43, 22, 11, 73, 25]
-    },
-    {
-      label: '2014',
-      values: [31, 28, 14, 8, 15, 21]
-    },]
-};
+  var m = [30, 10, 10, 85],
+    w = 960 - m[1] - m[3],
+    h = 3530 - m[0] - m[2];
 
-var chartWidth       = 300,
-    barHeight        = 20,
-    groupHeight      = barHeight * data.series.length,
-    gapBetweenGroups = 10,
-    spaceForLabels   = 150,
-    spaceForLegend   = 150;
+var x = d3.scale.linear().range([0, w]),
+    y = d3.scale.ordinal().rangeBands([0, h], 0.3);
 
-// Zip the series data together (first values, second values, etc.)
-var zippedData = [];
-for (var i=0; i<data.labels.length; i++) {
-  for (var j=0; j<data.series.length; j++) {
-    zippedData.push(data.series[j].values[i]);
-  }
+var xAxis = d3.svg.axis().scale(x).orient("top").tickSize(-h),
+    yAxis = d3.svg.axis().scale(y).orient("left").tickSize(0);
+
+var svg = d3.select("#chart").append("svg")
+    .attr("width", w + m[1] + m[3])
+    .attr("height", h + m[0] + m[2])
+  .append("g")
+    .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+d3.csv("mockJson/georgia_counties_vmt.csv", function(input) {
+
+  data = input;
+  // Parse numbers, and sort by value.
+  data.forEach(function(d) { d.value = +d.value; });
+  data.forEach(function (d) {d.n1 = +d.n1; })
+  data.sort(function(a, b) { return b.value - a.value; });
+
+
+  // Set the scale domain.
+  x.domain([0, d3.max(data, function(d) { return d.value; })]);
+  y.domain(data.map(function(d) { return d.name; }));
+  
+  change(); 
+
+}); // close d3.csv ...
+
+function change() {
+  d3.transition()
+    .duration(3750)
+    .each(redraw);
 }
 
-// Color scale
-var color = d3.scale.category20();
-var chartHeight = barHeight * zippedData.length + gapBetweenGroups * data.labels.length;
+function redraw() {
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(zippedData)])
-    .range([0, chartWidth]);
 
-var y = d3.scale.linear()
-    .range([chartHeight + gapBetweenGroups, 0]);
+  var nbar = svg.selectAll("g.nbar")
+      .data(data)
+      .attr("class", "nbar");
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .tickFormat('')
-    .tickSize(0)
-    .orient("left");
+  var nbarEnter = nbar.enter().insert("g")
+    .attr("class", "nbar")
+    .attr("transform", function (d) {return "translate(0," + (d.y0 = (9+y(d.name))) +")" ;});
 
-// Specify the chart area and dimensions
-var chart = d3.select(".chart")
-    .attr("width", spaceForLabels + chartWidth + spaceForLegend)
-    .attr("height", chartHeight);
+  nbarEnter.append("rect")
+    .attr("width", 0)
+    .attr("height", y.rangeBand()/2);
 
-// Create bars
-var bar = chart.selectAll("g")
-    .data(zippedData)
-    .enter().append("g")
-    .attr("transform", function(d, i) {
-      return "translate(" + spaceForLabels + "," + (i * barHeight + gapBetweenGroups * (0.5 + Math.floor(i/data.series.length))) + ")";
-    });
+  var nbarUpdate = d3.transition(nbar);
 
-// Create rectangles of the correct width
-bar.append("rect")
-    .attr("fill", function(d,i) { return color(i % data.series.length); })
+  nbarUpdate.select("rect")
+    .attr("width", function(d) { return x(d.n1); });
+
+  var bar = svg.selectAll("g.bar")
+      .data(data)
+      .attr("class", "bar");
+
+  var barEnter = bar.enter().insert("g")
     .attr("class", "bar")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
+    .attr("transform", function (d) {return "translate(0," + (d.y0 = (y(d.name))) +")" ;});
 
-// Add text label in bar
-bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("fill", "red")
-    .attr("dy", ".35em")
-    .text(function(d) { return d; });
+  initAxes();
 
-// Draw labels
-bar.append("text")
-    .attr("class", "label")
-    .attr("x", function(d) { return - 10; })
-    .attr("y", groupHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d,i) {
-      if (i % data.series.length === 0)
-        return data.labels[Math.floor(i/data.series.length)];
-      else
-        return ""});
+  barEnter.append("rect")
+    .attr("width", 0)
+    .attr("height", y.rangeBand()/2);
 
-chart.append("g")
-      .attr("class", "y axis")
-      .attr("transform", "translate(" + spaceForLabels + ", " + -gapBetweenGroups/2 + ")")
-      .call(yAxis);
+  var barUpdate = d3.transition(bar);
 
-// Draw legend
-var legendRectSize = 18,
-    legendSpacing  = 4;
+  barUpdate.select("rect")
+    .attr("width", function(d) { return x(d.value); })
+    .attr("fill", "orange");
 
-var legend = chart.selectAll('.legend')
-    .data(data.series)
-    .enter()
-    .append('g')
-    .attr('transform', function (d, i) {
-        var height = legendRectSize + legendSpacing;
-        var offset = -gapBetweenGroups/2;
-        var horz = spaceForLabels + chartWidth + 40 - legendRectSize;
-        var vert = i * height - offset;
-        return 'translate(' + horz + ',' + vert + ')';
-    });
+  svg.selectAll("g.y_axis").call(yAxis)
 
-legend.append('rect')
-    .attr('width', legendRectSize)
-    .attr('height', legendRectSize)
-    .style('fill', function (d, i) { return color(i); })
-    .style('stroke', function (d, i) { return color(i); });
+  svg.selectAll("g.x_axis").call(xAxis)
 
-legend.append('text')
-    .attr('class', 'legend')
-    .attr('x', legendRectSize + legendSpacing)
-    .attr('y', legendRectSize - legendSpacing)
-    .text(function (d) { return d.label; });
+
+
+} // end redraw()
+
+
+function initAxes()
+{
+
+  var initXscale = d3.scale.linear()
+    .range([0, 1])
+    .domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  var initXaxis = d3.svg.axis()
+    .scale(initXscale)
+    .tickSize(-h)
+    .orient("top");
+
+  svg.append("g")
+    .attr("class", "x_axis")
+    .call(initXaxis);   
+
+  var initYscale = d3.scale.ordinal()
+    .rangeBands([0, 1], 0.1)
+    .domain(data.map(function(d) { return d.name; }));
+
+  var initYaxis = d3.svg.axis()
+    .scale(initYscale)
+    .orient("left")
+    .tickSize(0);
+
+  svg.append("g")
+    .attr("class", "y_axis")
+    .call(initYaxis);
+
+} // initAxes()
+
+
   });
