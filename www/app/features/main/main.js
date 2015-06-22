de58c424c48fcb7b75fa87a89265796fd6e84534
Sameer Dhar <avielus@gmail.com>
2015-06-22 15:06:18 -0400

    Working typeahead

--- www/app/features/main/main.js
+++ www/app/features/main/main.js
@@ -8,7 +8,7 @@
  * Controller of the gapFront
  */
 angular.module('gapFront')
-  .controller('MainCtrl', function ($scope, APIService) {
+  .controller('MainCtrl', function ($scope, APIService, $http) {
     $scope.drugs = [];
     $scope.selectedDrug = '';
 
@@ -18,35 +18,38 @@ angular.module('gapFront')
       'Karma'
     ];
 
-    $scope.search = {text:''};
-
-    $scope.searchDrugs = function(viewValue){
-      console.log($scope.selectedDrug);
-      console.log('searchDrugs');
-      var text = viewValue;
-      //text = text.replace(' ','+');
-      var query = 'openfda.generic_name:'+text+' openfda.brand_name:'+text+' openfda.substance_name:'+text;
-      console.log(query);
-      APIService.queryDrugLabel(query,0,10).then(function(resp){
-        $scope.drugs = [];
-
-        for (var i in resp.results) {
-          var drug = {
-            brand_name: resp.results[i]['openfda']['brand_name'][0],
-            generic_name: resp.results[i]['openfda']['generic_name'][0],
-            substance_name: resp.results[i]['openfda']['substance_name'][0],
-            manufacturer_name: resp.results[i]['openfda']['manufacturer_name'][0]
-          };
-
-          $scope.drugs.push(drug);
-        }
-        console.log($scope.drugs);
-        return $scope.drugs;
-      },function(){
-        console.log('error!');
-      });
-      return $scope.drugs;
+    $scope.setSelectedDrug = function(drug) {
+      $scope.selectedDrug = drug;
+      $scope.drugs = [];
     };
 
+    $scope.searchDrugs = function() {
+      console.log('searchDrugs');
+      var text = $scope.search_text;
+      //text = text.replace(' ','+');
 
+      $scope.drugs = [];
+      if (text) {
+        var query = 'openfda.generic_name:' + text + ' openfda.brand_name:' + text + ' openfda.substance_name:' + text;
+        console.log(query);
+        APIService.queryDrugLabel(query, 0, 10).then(function (resp) {
+
+          for (var i in resp.results) {
+            var drug = {
+              brand_name: resp.results[i]['openfda']['brand_name'][0],
+              generic_name: resp.results[i]['openfda']['generic_name'][0],
+              substance_name: resp.results[i]['openfda']['substance_name'][0],
+              manufacturer_name: resp.results[i]['openfda']['manufacturer_name'][0]
+            };
+
+            $scope.drugs.push(drug);
+          }
+          console.log($scope.drugs);
+          return $scope.drugs;
+        }, function () {
+          console.log('error!');
+        });
+        return $scope.drugs;
+      }
+    }
   });
