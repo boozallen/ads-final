2b8da5b0abfa28a0fba050af599e79027d32b90e
Sami Mirimiri <mirimiri_sami@bah.com>
2015-06-23 12:32:49 -0400

    bigger chart

--- www/app/features/chart/chart.js
+++ www/app/features/chart/chart.js
@@ -53,22 +53,6 @@ angular.module('gapFront')
       $scope.terms = $scope.chart;
     };
 
-
-
-
-$scope.chart=[
-        {hour: 1,sales: 54},
-        {hour: 2,sales: 66},
-        {hour: 3,sales: 77},
-        {hour: 4,sales: 70},
-        {hour: 5,sales: 60},
-        {hour: 6,sales: 63},
-        {hour: 7,sales: 55},
-        {hour: 8,sales: 47},
-        {hour: 9,sales: 55},
-        {hour: 10,sales: 30}
-    ];
-
       //angular.element(document).ready();
       function createChart() {
         console.log($('#container'));
