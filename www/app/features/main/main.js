0b27de924b73f3f4ae4b3d877f4039357875d51f
Sameer Dhar <avielus@gmail.com>
2015-06-23 11:27:50 -0400

    Fixed duplicates thanks matt

--- www/app/features/main/main.js
+++ www/app/features/main/main.js
@@ -23,6 +23,7 @@ angular.module('gapFront')
     $scope.setSelectedDrug = function(drug) {
       $scope.selectedDrug = drug;
       $scope.drugs = [];
+      $scope.searchDrugEvents();
     };
 
     $scope.searchDrugs = function() {
@@ -32,23 +33,49 @@ angular.module('gapFront')
 
       if (text) {
         var query = 'openfda.generic_name:' + text + ' openfda.brand_name:' + text + ' openfda.substance_name:' + text;
-        console.log(query);
         APIService.queryDrugLabel(query, 0, 10).then(function (resp) {
           $scope.drugs = [];
           for (var i in resp.results) {
             if (resp.results[i]['openfda']['brand_name']){
-              console.log(resp.results[i]);
               var drug = {
                 brand_name: resp.results[i]['openfda']['brand_name'][0],
                 generic_name: resp.results[i]['openfda']['generic_name'][0],
+                lowercase_name: resp.results[i]['openfda']['brand_name'][0].toLowerCase(),
                 substance_name: resp.results[i]['openfda']['substance_name'] ? resp.results[i]['openfda']['substance_name'][0] : '',
+                effective_time: resp.results[i]['effective_time'] ? resp.results[i]['effective_time'] : '0',
                 manufacturer_name: resp.results[i]['openfda']['manufacturer_name'][0],
                 object: resp.results[i]
               };
             }
             $scope.drugs.push(drug);
           }
-          console.log($scope.drugs);
+
+          //console.log($scope.drugs);
+          $scope.drugs.sort(function (a, b) {
+            return a['effective_time'] + b['effective_time'];
+          });
+
+          var new_drugs = [];
+
+          for (var j in $scope.drugs) {
+            var isDuplicate = false;
+            for (var k in new_drugs) {
+              if (new_drugs[k]['lowercase_name'] == $scope.drugs[j][['lowercase_name']]) {
+                isDuplicate = true;
+                console.log('isDuplicate');
+                break;
+              }
+            }
+            if (!isDuplicate) {
+              new_drugs.push($scope.drugs[j]);
+            }
+          }
+
+
+          //console.log($scope.drugs);
+          $scope.drugs = new_drugs;
+          //console.log($scope.drugs);
+
           return $scope.drugs;
         }, function () {
           console.log('error!');
@@ -61,7 +88,6 @@ angular.module('gapFront')
 
     $scope.searchDrugEvents = function(){
       var query = 'patient.drug.medicinalproduct:'+$scope.selectedDrug.brand_name;
-      console.log(query);
       APIService.aggregateDrugEvent(query,'patient.reaction.reactionmeddrapt.exact').then(function(resp){
         IntegrationService.callIntegrationMethod('initChart',{results:resp.results});
       },function(){
