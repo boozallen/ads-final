840bb9c8b647867f0229ff106b5bec518fb2e98f
Sameer Dhar <avielus@gmail.com>
2015-06-23 16:33:23 -0400

    UI for adverse label effects

--- www/test/karma.conf.js
+++ www/test/karma.conf.js
@@ -34,6 +34,8 @@ module.exports = function(config) {
       'bower_components/angular-strap/dist/angular-strap.js',
       'bower_components/angular-strap/dist/angular-strap.tpl.js',
       'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/angular-bootstrap-checkbox/angular-bootstrap-checkbox.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/**/*.js',
