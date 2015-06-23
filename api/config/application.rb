8b5ca149f824c423a95b70dff7042a9161798bb9
Chris Kacerguis <kacerguis_christopher@bah.com>
2015-06-23 17:31:24 -0400

    fixed rubocop

--- api/config/application.rb
+++ api/config/application.rb
@@ -29,9 +29,8 @@ module Skeleton
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.action_dispatch.default_headers = {
-        'Access-Control-Allow-Origin' => '*',
-        'Access-Control-Request-Method' => %w{GET POST PUT DELETE OPTIONS}.join(",")
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => %w(GET POST PUT DELETE OPTIONS).join(',')
     }
-
   end
 end
