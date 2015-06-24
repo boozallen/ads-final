f31802e2de9d08d2128863047133c8142788be57
Chris Kacerguis <kacerguis_christopher@bah.com>
2015-06-24 10:54:01 -0400

    fixed rubocop issues

--- api/config/application.rb
+++ api/config/application.rb
@@ -28,10 +28,10 @@ module Skeleton
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, 'Rack::Cors' do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
   end
