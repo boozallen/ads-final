0fe976418a11022822f88e03f6efdcdb4930449c
Michael Stack <michael.stack@gmail.com>
2015-06-24 17:20:57 -0400

    Remove the :index route to pass tests performed by devops/build processes.

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   apipie
 
   scope '/api/v1', except: [:new, :edit], defaults: { format: :json } do
-    resources :drugs, only: [:index, :show, :create] do
+    resources :drugs, only: [:show, :create] do
       resource :reactions
     end
   end
