51ce5cf902d4562983a275bfa6ef20a1f452135c
joshi_saurab <saurabjoshi@C02KM0F0DR55.local>
2015-06-25 14:07:16 -0400

    registry script

---
+++ bin/registry.sh
@@ -0,0 +1,3 @@
+##Please note that this script will expect you to have /var/lib/registry path in your computer.
+##Change it to the path you want to store images in. 
+docker run -d -p 5000:5000  -v /var/lib/registry:/tmp/registry/repositories/library --restart=always --name registry registry:2
