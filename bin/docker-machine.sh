b65cb823956462ca9e389d8477363bbbefdd9f86
joshi_saurab <saurabjoshi@C02KM0F0DR55.local>
2015-06-25 14:27:58 -0400

    added docker-machine

---
+++ bin/docker-machine.sh
@@ -0,0 +1,3 @@
+wget https://github.com/docker/machine/releases/download/v0.3.0/docker-machine_linux-amd64
+mv docker-machine_linux-amd64 /usr/local/bin/docker-machine
+chmod +x /usr/local/bin/docker-machine
