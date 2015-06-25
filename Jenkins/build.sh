af48cd9f843562e2a2cd3b723fb754550a665033
joshi_saurab <saurabjoshi@C02KM0F0DR55.local>
2015-06-25 14:01:21 -0400

    deleted jenkins

--- Jenkins/build.sh
+++
@@ -1,7 +0,0 @@
-docker build -t frontend www
-docker run -p 9000:9000 -d frontend
-docker build -t database api/database/postgres
-docker run -p 5432:5432 -d database
-docker build -t api api
-docker run -p 3000:3000 -d api
- 
--- Jenkins/fe-test.sh
+++
@@ -1,5 +0,0 @@
-# run frontend unit and integration tests
-echo '****** STARTING PUSH TO REPO ********'
-CONTAINER_ID=$(sudo docker run -d $FRONTEND_IMAGE_NAME grunt test);
-sudo docker wait $CONTAINER_ID || {}
-sudo docker rm $CONTAINER_ID
