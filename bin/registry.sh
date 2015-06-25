4ef2a80a641504c1989593c22d6a99ca5c86c180
joshi_saurab <saurabjoshi@C02KM0F0DR55.local>
2015-06-25 14:30:42 -0400

    added comments

--- bin/registry.sh
+++ bin/registry.sh
@@ -1,3 +1,5 @@
-##Please note that this script will expect you to have /var/lib/registry path in your computer.
-##Change it to the path you want to store images in. 
+##This will run private docker repository on your machine. 
+
+#Please note that this script will expect you to have /var/lib/registry path in your computer.
+#Change it to the path you want to store images in. 
 docker run -d -p 5000:5000  -v /var/lib/registry:/tmp/registry/repositories/library --restart=always --name registry registry:2
