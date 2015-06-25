e88784496e176f29d24685935626ef2b7e24f14e
joshi_saurab <saurabjoshi@C02KM0F0DR55.local>
2015-06-25 13:32:30 -0400

    added build script

---
+++ Jenkins/build.sh
@@ -0,0 +1,6 @@
+docker build -t frontend www
+docker run -p 9000:9000 -d frontend
+docker build -t database api/database/postegres
+docker run -p 5432:5432 -d database
+docker build -t api api
+docker run -p 3000:3000 -d api
