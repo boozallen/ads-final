fab3feca6c6beece3cc0d06a16cdac6b4f8b7aa2
Sameer Dhar <avielus@gmail.com>
2015-06-26 16:24:09 -0400

    Fix questions post

--- api/app/controllers/effects_controller.rb
+++ api/app/controllers/effects_controller.rb
@@ -2,6 +2,6 @@ class EffectsController < ApplicationController
   api :POST, '/events', 'Record new events response to question.'
 
   def create
-    Effect.create!(drug_name: params[:drug_name], name: params[:effect].effect.medical_term, response: params[:response])
+    Effect.create!(drug_name: params[:drug_name], name: params[:effect], response: params[:response])
   end
 end
