23a6b999f441f6e1ea037cb29458d317feca41c6
Sameer Dhar <avielus@gmail.com>
2015-06-28 13:37:42 -0400

    Adding zipcode to leaders

--- www/app/features/leaderBoard/leaderBoard.js
+++ www/app/features/leaderBoard/leaderBoard.js
@@ -19,18 +19,23 @@ angular.module('gapFront')
         $scope.leaders = resp;
       });
 
-    }
+    };
 
-    
+    APIService.getLeadersApi().getList().then(function(resp) {
+      $scope.leaders = resp;
+    });
 
-    $scope.addLeader = function(name){
-      var post = {name: name};
+
+
+    $scope.addLeader = function(firstName, lastName, zipcode){
+      var post = {name: firstName + ' ' +  lastName, total: 5, zipcode: zipcode};
+      console.log(post);
       console.log("Adding leader");
       APIService.getLeadersApi().post(post).then(serviceError, serviceError);
-      APIService.getEffectsApi().post(post).then(serviceError, serviceError);
     };
 
     function serviceError(error) {
+      console.log(error);
     }
 
     // $scope.leaders = ["Steve S.", "Jason P.", "Sami M.", "Sameer D.", "Fred J.", "George B.", "Shelly R."];
